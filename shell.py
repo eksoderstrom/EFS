b33e62a37e9c35ba6fc37070508679f68985beda
eks <ericsoderstrom@gmail.com>
2013-12-02 18:35:30 -0500

    basic working shell prototype for encrypting and decrypting unshared files

---
+++ shell.py
@@ -0,0 +1,34 @@
+import cmd
+import client
+
+class Shell(cmd.Cmd):
+
+    def do_xfer(self, line):
+        p = line.split()
+        if len(p) != 1:
+            print("Usage: xfer source")
+        client.xfer(p[0])
+
+
+    def do_mv(self, line):
+        p = line.split()
+        if len(p) != 2:
+            print("Usage: mv source target")
+        client.mv(p[0], p[1])
+
+    def do_rm(self, line):
+        pass
+
+    def do_enc(self, line):
+        p = line.split()
+        client.enc(p[0])
+
+    def do_dec(self, line):
+        p = line.split()
+        client.dec(p[0])
+    
+    def do_EOF(self, line):
+        return True
+
+if __name__ == '__main__':
+    Shell().cmdloop()

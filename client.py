aa236dc8910e57d2ea0170a267e9ed2a6b3df1e2
tifftang <fable@mit.edu>
2013-12-04 12:41:01 -0500

    added many functions, but haven't linked them together

--- client.py
+++ client.py
@@ -1,9 +1,97 @@
-import xmlrpc.client, random, struct, os, hashlib
+import xmlrpc.client, random, struct, os, hashlib, sys
 from Crypto.Cipher import AES
+#RSA imports
+import Crypto.PublicKey.RSA as RSA
+import Crypto.Random.OSRNG.posix as Nonce
+#MAC import
+import hmac
+from os import urandom
+"""TODO
+    how to associate private keys with files
+    """
 
-#Client
 s = xmlrpc.client.ServerProxy('http://localhost:8000')
 key = '0123456789abcdef'    #Key should of course not be hard-coded, and should be stored on disk. placeholder for now
+
+#Macros
+AES_KEY_SIZE = 256
+RSA_KEY_SIZE = 1024
+FILE_HEADER_NONCE_SIZE = 32
+WARNING = 'DO NOT DELETE THIS FILE. USED IN ENCRYPTED FILE SYSTEM.'
+
+class Client():
+    def __init__(self, username, password):
+        pass
+
+#########################################################
+# 
+#
+#########################################################
+def generate_nonce(size):
+    """ Generates a random value using a secure
+        random number generator from os.urandom
+
+        size: number of bytes
+    """
+    #return Nonce.new().read(size) -> this cannot be used by windows
+    return os.urandom(size)
+def generate_rsa_key(size):
+    """ Generate a fresh, new RSA key object
+    where nonce is the random function and size is
+    the size of the key object
+    """
+    nonce = os.urandom
+    key = RSA.generate(size, nonce)
+    return key
+
+def encrypt_aes_key(key):
+    """ Encrypts AES key with another
+        user's public key
+
+        key: public key of person
+    """
+    pass
+
+def decrypt_aes_key(public_rsa_key, encrypted_aes_key):
+    """ Decrypts AES key with user's public
+        key"""
+    pass
+
+def generate_aes_key():
+    key = generate_nonce(AES_KEY_SIZE)
+    return key
+
+def export_rsa_key(filename, key, passphrase=None):
+    """ Save a copy of our rsa key.
+        The extension '.pub' is used
+        for public keys and '.pri'
+        is used for private keys.
+    """
+    try:
+        f = open(filename, 'w')
+        f.write(key.exportKey())
+        f.close()
+    except IOError as e:
+        print(e)
+
+
+def load_rsa_key(filename, passphrase=None):
+    """ Retrieve our rsa key from file
+
+        filename: name of file containing
+        public-private key pair
+    """
+    try:
+        f = open(filename)
+        key = RSA.importKey(f.read())
+        f.close()
+        return key
+    except IOError as e:
+        print (e)
+
+def send_public_key(key):
+    key.publickey().exportKey()
+
 
 def xfer(source):
     with open(source, "rb") as handle:
@@ -14,12 +102,55 @@ def mv(source, dst):
     pass
 
 def enc(fn):
-    encrypt_file(fn)
+    new_in_filename = add_file_header(fn)
+    encrypt_file(new_in_filename)
 
 def dec(fn):
     decrypt_file(fn, "/Users/eks/Desktop/decrypted")
 
+def store_file_log(in_filename, filesize, gen_count, mac):
+    """ Stores information about file on the client-size.
 
+        filesize:
+            Size of the file.
+
+        in_filename:
+            Name of the input file
+
+        out_filename:
+            '<in_filename>.txt' will always be used.
+
+        gen_count:
+            The version of this file. Starting value is 1.
+    """
+    out_filename = in_filename + '.txt'
+    nonce = os.urandom(32)
+    with open(out_filename, 'wb') as outfile:
+        outfile.write(in_filename)
+        outfile.write(filesize)
+        outfile.write(gen_count)
+        outfile.write(mac)
+
+def add_file_header(in_filename, key):
+    """ Adds a file header to the front of the file of the format
+        FILEHEADER_START
+        FILEHEADER_END
+
+        key: aes private key
+    """
+
+    start = 'FILEHEADER_START'
+    end = 'FILEHEADER_END'
+    nonce = generate_nonce(FILE_HEADER_NONCE_SIZE)
+    mac = hmac.new(key, message, hashlib.sha256).hexdigest()
+
+def has_file_header(in_filename):
+    """ Checks if this file has a file header
+        placed by our encrypted file system
+    """
+    
+    
+#Taken from http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/
 def encrypt_file(in_filename, out_filename=None, chunksize=64*1024):
     """ Encrypts a file using AES (CBC mode) with the
         given key.
@@ -41,6 +172,7 @@ def encrypt_file(in_filename, out_filename=None, chunksize=64*1024):
             sizes can be faster for some files and machines.
             chunksize must be divisible by 16.
     """
+        
 
     if not out_filename:
         out_filename = in_filename + '.encrypted'
@@ -63,6 +195,7 @@ def encrypt_file(in_filename, out_filename=None, chunksize=64*1024):
 
                 outfile.write(encryptor.encrypt(chunk))
 
+#Taken from http://eli.thegreenplace.net/2010/06/25/aes-encryption-of-files-in-python-with-pycrypto/
 def decrypt_file(in_filename, out_filename=None, chunksize=64*1024):
     """ Decrypts a file using AES (CBC mode) with the
         given key. Parameters are similar to encrypt_file,
